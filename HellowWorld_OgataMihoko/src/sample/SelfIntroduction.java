8995c0922abe23ed06f1381efd196b3528503154
onidzukatakashi <onidzukatakashi@localhost>
2025-11-04 17:43:29 +0900

    Java導入

---
+++ HellowWorld_OgataMihoko/src/sample/SelfIntroduction.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "尾形　美帆子";
+		//年齢
+		int age = 27;
+		//趣味
+		String hobby = "３DCG制作・イラストレーション";
+		
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		
+		System.out.println("私の名前は" + name + " です。 ");
+		System.out.println("年齢は" + age + " 歳です。 ");
+		System.out.println("趣味は" + hobby + " です。 ");
+		
+				
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
